a80fdaf6ecdd5e9e4cb6959e42349e80c7bfd0de
Bibi Arezo Halimi <bahalimi@apsl.net>
2023-08-16 12:48:30 +0200

    loading_course_data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,7 +1,23 @@
+
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
 </template>
-<script setup></script>
+
+<script setup>
+const course = useCourse();
+console.log(course)
+const route = useRoute();
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
