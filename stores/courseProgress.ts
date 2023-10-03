e5f9802ef8e3e812f8218a3a39c4a2f993d28e49
Bibi Arezo Halimi <bahalimi@apsl.net>
2023-10-03 12:33:57 +0200

    6-4 Add Endpoint to Update Progress

--- stores/courseProgress.ts
+++ stores/courseProgress.ts
@@ -1,55 +1,75 @@
 import { defineStore } from 'pinia';
 
+export const useCourseProgress = defineStore(
+  'courseProgress',
+  () => {
+    // Initialize progress from local storage
+    const progress = ref<any>({});
+    const initialized = ref(false);
 
-interface Progress {
-  [chapter: string]: {
-    [lesson: string]: boolean;
-  };
-}
+    async function initialize() {
+      // If the course has already been initialized, return
+      if (initialized.value) return;
+      initialized.value = true;
 
-export const useCourseProgress = defineStore('courseProgress', () => {
-  // Initialize progress from local storage
-  const progress: Ref<Progress> = useLocalStorage('progress', {});
-  const initialized = ref(false);
-
-  async function initialize() {
-    // If the course has already been initialized, return
-    if (initialized.value) return;
-    initialized.value = true;
-
-    // TODO: Fetch user progress from endpoint (lesson 6-5)
-  }
-
-  // Toggle the progress of a lesson based on chapter slug and lesson slug
-  const toggleComplete = async (chapter?: string, lesson?: string) => {
-    // If there's no user we can't update the progress
-    const user = useSupabaseUser();
-    if (!user.value) return;
-
-    // Grab chapter and lesson slugs from the route if they're not provided
-    if (!chapter || !lesson) {
-      const {
-        params: { chapterSlug, lessonSlug },
-      } = useRoute();
-      chapter = chapterSlug as string;
-      lesson = lessonSlug as string;
+      // TODO: Fetch user progress from endpoint (lesson 6-5)
     }
 
-    // Get the current progress for the lesson
-    const currentProgress = progress.value[chapter]?.[lesson];
+    // Toggle the progress of a lesson based on chapter slug and lesson slug
+    const toggleComplete = async (
+      chapter: string,
+      lesson: string
+    ) => {
+      // If there's no user we can't update the progress
+      const user = useSupabaseUser();
+      if (!user.value) return;
 
-    // Optimistically update the progress value in the UI
-    progress.value[chapter] = {
+      // Grab chapter and lesson slugs from the route if they're not provided
+      if (!chapter || !lesson) {
+        const {
+          params: { chapterSlug, lessonSlug },
+        } = useRoute();
+        chapter = chapterSlug as string;
+        lesson = lessonSlug as string;
+      }
+
+      // Get the current progress for the lesson
+      const currentProgress =
+        progress.value[chapter]?.[lesson];
+
+      // Optimistically update the progress value in the UI
+      progress.value[chapter] = {
         ...progress.value[chapter],
         [lesson]: !currentProgress,
       };
-    
-    // TODO: Update in DB (lesson 6-4)
-  };
 
-  return {
-    initialize,
-    progress,
-    toggleComplete,
-  };
-});
+      // Update the progress in the DB
+      try {
+        await $fetch(
+          `/api/course/chapter/${chapter}/lesson/${lesson}/progress`,
+          {
+            method: 'POST',
+            // Automatically stringified by ofetch
+            body: {
+              completed: !currentProgress,
+            },
+          }
+        );
+      } catch (error) {
+        console.error(error);
+
+        // If the request failed, revert the progress value
+        progress.value[chapter] = {
+          ...progress.value[chapter],
+          [lesson]: currentProgress,
+        };
+      }
+    };
+
+    return {
+      initialize,
+      progress,
+      toggleComplete,
+    };
+  }
+);
